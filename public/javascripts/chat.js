96434c1a9a02e2d1629da2db6181c98f37016e0a
PJ Kim <pj@kakao.com>
2013-06-24 15:26:07 +0900

    use jquery

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,34 +1,41 @@
-window.onload = function() {
+$(document).ready(function () {
 
   var messages = [];
   var socket = io.connect('http://localhost:3700');
-  var field = document.getElementById("field");
-  var sendButton = document.getElementById("send");
-  var content = document.getElementById("content");
-  var name = document.getElementById("name");
+  var field = $("#field");
+  var sendButton = $("#send");
+  var content = $("#content");
+  var name = $("#name");
 
   socket.on('message', function (data) {
-    if(data.message) {
+    console.log(data);
+    if (data.message) {
       messages.push(data);
       var html = '';
-      for(var i=0; i<messages.length; i++) {
-        console.log(messages[i].username);
+      for (var i = 0; i < messages.length; i++) {
         html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
         html += messages[i].message + '<br />';
       }
-      content.innerHTML = html;
+      content.html(html);
+      content.scrollTop(content[0].scrollHeight);
     } else {
       console.log("There is a problem:", data);
     }
   });
 
-  sendButton.onclick = function() {
-    if(name.value == '') {
+  sendButton.click(sendMessage = function () {
+    if (!name.val()) {
       alert('Please type your name!')
     } else {
-      var text = field.value;
-      socket.emit('send', { message: text, username: name.value });
+      socket.emit('send', { message: field.val(), username: name.val() });
+      field.val('');
     }
-  };
+  });
 
-}
+  $("#field").keyup(function (e) {
+    if (e.keyCode == 13) {
+      sendMessage();
+    }
+  });
+
+});
